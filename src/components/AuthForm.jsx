e18e10727ef594b7d8c4cf8f46609ccd7faf6ee9
Ajayi Similoluwa <ajayi.similoluwa@lmu.edu.ng>
2024-05-03 11:28:48 +0100

    signup page

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -40,7 +40,7 @@ const AuthForm = () => {
             <div className="flex flex-col md:flex-row h-screen">
               <div className="w-full py-52 md:py-8 md:w-1/2 bg-gray-100 flex justify-center items-center relative">
                 <img src={Logo} alt="Logo" className="absolute left-7 top-10 w-24 h-18" />
-                <div className="md:w-[500px] w-[300px] px-4">
+                <div className="md:w-[500px] w-[370px] px-4">
                   <h2 className="w-full mx-auto text-gray-700 text-5xl font-bold font-['Inter'] leading-[48px] mb-20">{isSignUp ? 'Sign Up' : 'Sign In'}</h2>
                   {error && <p className="text-red-500 mb-4">{error}</p>}
                   <form onSubmit={handleSubmit}>
