7cc6e1464d17eedc5dec4aeb8dbcafd935c41aad
Ajayi Similoluwa <ajayi.similoluwa@lmu.edu.ng>
2024-05-03 11:58:36 +0100

    signup page

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -70,13 +70,39 @@ const AuthForm = () => {
                       onChange={isSignUp ? handleSignUpChange : handleSignInChange}
                       className="w-full p-3 mb-4 border border-gray-300 rounded-3xl focus:outline-none"
                     />
-                    <button
-                      type="submit"
-                      className="w-full bg-green-800 text-white py-2 hover:bg-green-700 rounded-xl"
-                      disabled={isLoading}
-                    >
-                      {isLoading ? 'Loading...' : isSignUp ? 'Sign Up' : 'Sign In'}
-                    </button>
+                   <button
+  type="submit"
+  className="w-full bg-green-800 text-white py-2 hover:bg-green-700 rounded-xl flex items-center justify-center"
+  disabled={isLoading}
+>
+  {isLoading ? (
+    <svg
+      className="animate-spin h-5 w-5 mr-3"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+    
+  ) : isSignUp ? (
+    'Sign Up'
+  ) : (
+    'Sign In'
+  )}
+</button>
                   </form>
                   <p className="mt-4 text-center">
                     {isSignUp ? (
