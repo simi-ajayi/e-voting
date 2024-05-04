1e437a4e22bed7fc3a488e14baac8b30ec2faace
Ajayi Similoluwa <ajayi.similoluwa@lmu.edu.ng>
2024-05-04 16:04:21 +0100

    vote number fix

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -8,19 +8,21 @@ import Logo from '../assets/logo.png';
 import AdminPage from './AdminPage';
 
 const AuthForm = () => {
-  const [isSignUp, setIsSignUp] = useState(false); // Set isSignUp to false initially
-  const { signUpData, handleSignUpChange, handleSignUp } = useSignUp();
-  const { signInData, handleSignInChange, handleSignIn, error, isLoading } = useSignIn();
+  const [isSignUp, setIsSignUp] = useState(false);
+  const { signUpData, handleSignUpChange, handleSignUp, error: signUpError, isLoading: isSignUpLoading } = useSignUp();
+  const { signInData, handleSignInChange, handleSignIn, error: signInError, isLoading: isSignInLoading } = useSignIn();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (isSignUp) {
-      await handleSignUp();
-      setIsAuthenticated(true);
+      const success = await handleSignUp();
+      if (success) {
+        setIsAuthenticated(true);
+      }
     } else {
-      const { isAdmin: admin } = await handleSignIn(); // Destructure isAdmin from the return value
+      const { isAdmin: admin } = await handleSignIn();
       setIsAdmin(admin);
       setIsAuthenticated(true);
     }
@@ -47,7 +49,7 @@ const AuthForm = () => {
                 <img src={Logo} alt="Logo" className="absolute left-7 top-10 w-24 h-18" />
                 <div className="md:w-[500px] w-[370px] px-4">
                   <div className='w-full pl-[25%]'><h2 className="w-full  text-gray-700 text-5xl font-bold font-['Inter'] leading-[48px] mb-20">{isSignUp ? 'Sign Up' : 'Sign In'}</h2></div>
-                  {error && <p className="text-red-500 mb-4">{error}</p>}
+                  {(isSignUp ? signUpError : signInError) && <p className="text-red-500 mb-4">{isSignUp ? signUpError : signInError}</p>}
                   <form onSubmit={handleSubmit}>
                     {isSignUp && (
                       <input
@@ -75,12 +77,37 @@ const AuthForm = () => {
                       onChange={isSignUp ? handleSignUpChange : handleSignInChange}
                       className="w-full p-3 mb-4 border border-gray-300 rounded-3xl focus:outline-none"
                     />
-                   <button
-                      type="submit"
-                         className="w-full bg-[#2e9196] text-white py-2 hover:bg-[#38A8AD] rounded-xl flex items-center justify-center"
-                     disabled={isLoading}
+                    <button
+  type="submit"
+  className="w-full bg-[#2e9196] text-white py-2 hover:bg-[#38A8AD] rounded-xl flex items-center justify-center"
+  disabled={isSignUp ? isSignUpLoading : isSignInLoading}
 >
-  {isLoading ? (
+  {isSignUp ? (
+    isSignUpLoading ? (
+      <svg
+        className="animate-spin h-5 w-5 mr-3"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    ) : (
+      'Sign Up'
+    )
+  ) : isSignInLoading ? (
     <svg
       className="animate-spin h-5 w-5 mr-3"
       xmlns="http://www.w3.org/2000/svg"
@@ -101,13 +128,11 @@ const AuthForm = () => {
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
       ></path>
     </svg>
-    
-  ) : isSignUp ? (
-    'Sign Up'
   ) : (
     'Sign In'
   )}
 </button>
+
                   </form>
                   <p className="mt-4 text-center">
                     {isSignUp ? (
@@ -135,7 +160,6 @@ const AuthForm = () => {
                 </div>
               </div>
               <div className="w-full md:w-1/2 bg-gradient-to-tr from-gray-500 via-gray-300 to-gray-500 flex justify-center items-center md:block relative">
-                {/* <img src={Logo} alt="Logo" className="absolute top-4 left-4 w-12 h-12" /> */}
                 <div className="w-[525px] h-[334px] relative bg-slate-50 rounded-[10px] md:block hidden mx-auto mt-48">
                   <div className="w-[386px] h-[103px] left-[44px] top-[109px] absolute text-gray-400 text-base font-medium font-['Inter'] leading-7">
                     Cribbers Awards streamlines the voting process for your award show and events, making it easy for participants to cast their votes and contribute to the selection of winners.
@@ -153,7 +177,6 @@ const AuthForm = () => {
         path="/account"
         element={isAuthenticated && !isAdmin ? <UserAccountPage /> : <Navigate to="/" />}
       />
-      {/* Add a new route for the admin page */}
       <Route
         path="/admin"
         element={isAuthenticated && isAdmin ? <AdminPage /> : <Navigate to="/" />}
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,24 +1,58 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 const useSignUp = () => {
   const [signUpData, setSignUpData] = useState({
-    username: '',
-    email: '',
-    password: '',
+    username: 'bishopolumayor',
+    email: 'bishop@example.com',
+    password: '#Million1',
   });
 
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleSignUpChange = (e) => {
-    setSignUpData({ ...signUpData, [e.target.name]: e.target.value });
+    setSignUpData({
+      ...signUpData,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const handleSignUp = () => {
-    // Perform signup logic here (e.g., send data to server)
-    console.log('Sign up data:', signUpData);
-    // Reset form data
-    setSignUpData({ username: '', email: '', password: '' });
+  const handleSignUp = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await axios.post('http://localhost:3050/api/users/register', signUpData);
+      if (response.data.status && response.data.statusCode === 201) {
+        console.log('User created:', response.data.data.userId);
+        // Reset form data
+        setSignUpData({
+          username: 'bishopolumayor',
+          email: 'bishop@example.com',
+          password: '#Million1',
+        });
+        return true; // Return true to indicate successful sign-up
+      } else {
+        setError('Failed to create user. Please try again.');
+        return false; // Return false to indicate sign-up failure
+      }
+    } catch (error) {
+      console.error('Error creating user:', error);
+      setError('An error occurred while creating user. Please try again.');
+      return false; // Return false to indicate sign-up failure
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  return { signUpData, handleSignUpChange, handleSignUp };
+  return {
+    signUpData,
+    handleSignUpChange,
+    handleSignUp,
+    error,
+    isLoading,
+  };
 };
 
 export default useSignUp;
